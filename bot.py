d51d6218229449b2b2322cbfb4ab7793bc3813e2
s82520773-blip <s82520773@gmail.com>
2026-03-12 00:40:18 -0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+TOKEN = "TU_TOKEN_AQUI"
+
+def start(update, context):
+    update.message.reply_text(
+        "🎬 Bienvenido\n\n¿Que puedo hacer por ti?\n\nEscribe el nombre de la película que buscas."
+    )
+
+def buscar(update, context):
+    texto = update.message.text.lower()
+
+    if "gigantes" in texto:
+        update.message.reply_text(
+            "😀 Disponible\n\n🎬 Gigantes de acero\n📅 2011\n🎥 1080p\n\n💰 Precio $11"
+        )
+    else:
+        update.message.reply_text("😕 No encontré esa película.")
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text, buscar))
+
+    updater.start_polling()
+    updater.idle()
+
+main()
